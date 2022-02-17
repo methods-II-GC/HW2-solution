55b1a0213c812aa7124a58fd2a3d3b059a27eeba
Kyle Gorman <kylebgorman@gmail.com>
2022-02-17 16:07:39 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+See [`run`](run).
+
+The following shows the file sizes obtained:
+
+```
+$ wc -l train.txt dev.txt test.txt
+   338799 dev.txt
+   338808 test.txt
+  2710392 train.txt
+  6775998 total
+```
---
+++ split.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Creates an 80%/10%/10% split, assuming each line is an example."""
+
+import argparse
+import random
+
+
+def main(args: argparse.Namespace) -> None:
+    # It's usually bad to slurp up a single file at once. But the alternative
+    # here is rather complicated.
+    lines: list[str] = []
+    with open(args.input, "r") as source:
+        for line in source:
+            lines.append(line.rstrip())
+    random.seed(args.seed)
+    # Note that this shuffle function returns nothing: it "works in place".
+    random.shuffle(lines)
+    decile = len(lines) // 10
+    with open(args.train, "w") as sink:
+        for line in lines[:8 * decile]:
+            print(line, file=sink)
+    with open(args.dev, "w") as sink:
+      for line in lines[8 * decile:9 * decile]:
+            print(line, file=sink)
+    with open(args.test, "w") as sink:
+      for line in lines[9 * decile:]:
+            print(line, file=sink)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--seed", "-s", required=True, type=int, help="random seed")
+    parser.add_argument("input", help="path to input file")
+    parser.add_argument("train", help="path to output training set file")
+    parser.add_argument("dev", help="path to output development set file")
+    parser.add_argument("test", help="path to output testing set file")
+    main(parser.parse_args())
